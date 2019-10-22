faa67e161af164b0ddd13697b1f4ce6f4bf1a685
allisonchen23 <achen227@yahoo.com>
2019-10-22 00:02:41 -0700

    added navigation packages and created navigation js file. Need to test

--- display_screen/navigation.js
+++ display_screen/navigation.js
@@ -1,13 +1,36 @@
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+// import {createAppContainer} from 'react-navigation';
+// import {createStackNavigator} from 'react-navigation-stack';
 
-const MainNavigator = createStackNavigator(
-    {
-    display: {screen: DisplayScreen},
-    dummy: {screen: DummyScreen},
+// const MainNavigator = createStackNavigator(
+//     {
+//     display: {screen: DisplayScreen},
+//     dummy: {screen: DummyScreen},
+//   }
+//   );
+  
+//   const Navigate = createAppContainer(MainNavigator);
+  
+//   export default Navigate;
+
+import React from 'react';
+import { View, Text } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+      </View>
+    );
   }
-  );
-  
-  const Navigate = createAppContainer(MainNavigator);
-  
-  export default Navigate;
+}
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+});
+
+export default createAppContainer(AppNavigator);
--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import Display from './display_screen/display';
 import Dummy from './display_screen/dummy';
+import AppNavigator from './display_screen/navigation';
 import {name as appName} from './app.json';
 
- AppRegistry.registerComponent(appName, () => Display);
+ AppRegistry.registerComponent(appName, () => AppNavigator);
 //AppRegistry.registerComponent(appName, () => Navigate);
