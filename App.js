50beef7b8837565b3edef6a21587e5565060dbfa
Bryan Pan <bryanpan342@gmail.com>
2019-10-30 21:51:40 -0700

    workable changes

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
@@ -13,7 +14,7 @@ import {
 var _ = require('lodash');
 import BluetoothSerial from 'react-native-bluetooth-serial'
 import Dialogflow, { Dialogflow_V2 } from "react-native-dialogflow"
- 
+
 export default class App extends Component<{}> {
   constructor (props) {
     super(props)
@@ -28,83 +29,83 @@ export default class App extends Component<{}> {
       audioLevel: 0,
       text: ''
     }
- 
+
     this.count = 0
- 
+
     Dialogflow.setConfiguration(
       "e0712137bce540b8bd4c4b5d5840be07", Dialogflow.LANG_ENGLISH
     );
- 
+
     Dialogflow_V2.setConfiguration(
       "ya29.c.El93BfD5CZBWUrPJAyUdfWuHkmdzyjo0seL56z-reryvae4j8oVwLO38ARqrpj0Q5LxeUTndPLrA6GYPx4CyrlVmJRKvq_Dtwl9OO0zxiWTVK7boCV-aCVJVHbaFSGstgA",
       Dialogflow_V2.LANG_ENGLISH,
       'testv2-3b5ca'
     );
- 
+
   }
   componentWillMount(){
- 
+
     Promise.all([
       BluetoothSerial.isEnabled(),
       BluetoothSerial.list()
     ])
     .then((values) => {
       const [ isEnabled, devices ] = values
- 
+
       this.setState({ isEnabled, devices })
     })
- 
+
     BluetoothSerial.on('bluetoothEnabled', () => {
- 
+
       Promise.all([
         BluetoothSerial.isEnabled(),
         BluetoothSerial.list()
       ])
       .then((values) => {
         const [ isEnabled, devices ] = values
-        this.setState({  devices })
+        this.setState({ isEnabled, devices })
       })
- 
+
       BluetoothSerial.on('bluetoothDisabled', () => {
- 
+
          this.setState({ devices: [] })
- 
+
       })
       BluetoothSerial.on('error', (err) => console.log(`Error: ${err.message}`))
- 
+
     })
- 
+
   }
   _onPressButton(){
- 
+
     this.count = 0
     // V1
     Dialogflow.onListeningStarted(() => {
       this.setState({ listeningState: "started" });
     });
- 
+
     Dialogflow.onListeningCanceled(() => {
       this.setState({ listeningState: "canceled" });
     });
- 
+
     Dialogflow.onListeningFinished(() => {
       this.setState({ listeningState: "finished" });
     });
- 
+
     Dialogflow.onAudioLevel(level => {
       this.setState({ audioLevel: level });
     });
- 
+
     Dialogflow.startListening(result => {
- 
+
       console.log(result.result.resolvedQuery);
       const text = result.result.resolvedQuery
       this.setState({ result: text });
- 
+
       if(this.count == 0){
         // code to send signal
         if ((text.indexOf("switch") >= 0) && (text.indexOf(" on ") >= 0) && (text.split(' ').length >= 3)){
- 
+
           var thenum = text.match(/\d/g);
           thenum = thenum.join("");
           //console.warn(thenum);
@@ -126,11 +127,11 @@ export default class App extends Component<{}> {
                   break;
             }
           }
- 
+
           this.count = this.count + 1
         }
       }
- 
+
     }, error => {
       this.setState({ result: JSON.stringify(error) });
     });
@@ -143,13 +144,13 @@ export default class App extends Component<{}> {
     BluetoothSerial.connect(device.id)
     .then((res) => {
       console.log(`Connected to device ${device.name}`);
- 
+
       ToastAndroid.show(`Connected to device ${device.name}`, ToastAndroid.SHORT);
     })
     .catch((err) => console.log((err.message)))
   }
   _renderItem(item){
- 
+
     return(<TouchableOpacity onPress={() => this.connect(item.item)}>
             <View style={styles.deviceNameWrap}>
               <Text style={styles.deviceName}>{ item.item.name ? item.item.name : item.item.id }</Text>
@@ -161,13 +162,13 @@ export default class App extends Component<{}> {
     .then((res) => this.setState({ isEnabled: true }))
     .catch((err) => Toast.showShortBottom(err.message))
   }
- 
+
   disable () {
     BluetoothSerial.disable()
     .then((res) => this.setState({ isEnabled: false }))
     .catch((err) => Toast.showShortBottom(err.message))
   }
- 
+
   toggleBluetooth (value) {
     if (value === true) {
       this.enable()
@@ -200,7 +201,7 @@ export default class App extends Component<{}> {
     .catch((err) => console.log(err.message))
   }
   render() {
- 
+
     return (
       <View style={styles.container}>
       <View style={styles.toolbar}>
@@ -223,10 +224,10 @@ export default class App extends Component<{}> {
           keyExtractor={item => item.id}
           renderItem={(item) => this._renderItem(item)}
         />
- 
+
         <Text style={{ fontSize:15, alignSelf: "center"}}>Message Sent :</Text>
         <Text style={{ fontSize:20, color: "black", fontWeight: "bold", textAlign: "center"}}>{this.state.result}</Text>
- 
+
         <TouchableOpacity onPress={this._onPressButton.bind(this)}>
           <Image
             style={{width: 100, height: 100, alignSelf:"center"}}
@@ -237,7 +238,7 @@ export default class App extends Component<{}> {
     );
   }
 }
- 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
