9d2b78f1b4d3b84efab9da434cf406d58e676d65
Param Shah <paramshah09@gmail.com>
2019-11-09 17:05:30 -0800

    Deleted the dummy page and resolved index.js where dummy.js was called

--- index.js
+++ index.js
@@ -5,9 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import Display from './display_screen/display';
-import Dummy from './display_screen/dummy';
 import AppNavigator from './display_screen/navigation';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
-
