f72b6d2bedf6a76c5ea38fef3ecc60e6aff197e1
Connie Chen <connie90503@gmail.com>
2019-11-25 22:28:40 -0800

    persistent strength data working

--- index.js
+++ index.js
@@ -4,10 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-<<<<<<< HEAD
-=======
-import Display from './display_screen/display';
->>>>>>> master
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
