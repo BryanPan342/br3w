09d8a4e4beabff3e1d273c2d8ec3247c57837b42
Bryan Pan <bryanpan342@gmail.com>
2019-12-06 09:50:49 -0800

    updated arduino code

--- arduino_code/main/coffee_dispenser_servo_motor.ino
+++ arduino_code/main/coffee_dispenser_servo_motor.ino
@@ -1,12 +1,12 @@
 
 void dispense_coffee(int grams) 
 {
-    Serial.println("Dispensing coffee");
+    Serial.println("c");
     int rotations = grams/4;
     for(int i = 0; i < rotations; i++) {
-    myservo.write(SERVO_START + SERVO_TURN);
-    delay(3000);
-    myservo.write(SERVO_START);
-    delay(10000);
-  }
+      myservo.write(SERVO_START + SERVO_TURN);
+      delay(3000); 
+      myservo.write(SERVO_START);
+      delay(5000);
+    }
 }
